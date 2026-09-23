bf9bde5fe8e8a9ffcb0570cc3b50d1328f8adaa5
camila24513 <camila24513@cbtis75.edu.mx>
2026-09-23 08:06:25 -0600

    Maquetado de catálogo de Studio Ghibli con Bootstrap y SweetAlert2 integrado por Camila

--- StudioGhibli/src/app/componentes/list-films/list-films.ts
+++ StudioGhibli/src/app/componentes/list-films/list-films.ts
@@ -1,9 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { IFilter } from '../../modelo/filter-model';
+import { FormsModule } from '@angular/forms';
+import { FilmService } from '../../services/film-service';
+import { IFilm } from '../../modelo/film-model';
+import { RouterLink } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
-  imports: [],
   selector: 'app-list-films',
-  styleUrl: './list-films.css',
-  templateUrl: './list-films.html',
+  standalone: true,
+  imports: [FormsModule, RouterLink],
+  templateUrl: './list-films.html'
 })
-export class ListFilms {}
+export class ListFilms {
+  private filmService = inject(FilmService);
+
+  public listaPeliculas: IFilm[] = [];
+  public filter: IFilter = { searchBy: 'title', value: '' };
+
+  ngOnInit() {
+    this.filterData();
+  }
+
+  filterData() {
+    this.filmService.getFilms(this.filter).subscribe({
+      next: (data) => {
+        this.listaPeliculas = data;
+        if (data.length === 0) {
+          Swal.fire({
+            title: 'Sin resultados',
+            text: 'No se encontraron películas de Studio Ghibli con ese filtro',
+            icon: 'warning',
+            confirmButtonText: 'Aceptar'
+          });
+        }
+      },
+      error: () => {
+        this.listaPeliculas = [];
+        Swal.fire({
+          title: 'Error',
+          text: 'Ocurrió un error al consultar la API de Studio Ghibli',
+          icon: 'error',
+          confirmButtonText: 'Cerrar'
+        });
+      }
+    });
+  }
+}
